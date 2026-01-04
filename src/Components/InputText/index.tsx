a72f69ee3274f3252b9011e19ee73a29a2cffe67
GiselleBarbosa <g88barbosa@gmail.com>
2026-01-04 17:37:10 -0300

    feat: adiciona componentes de Tag e Input

---
+++ src/Components/InputText/index.tsx
@@ -0,0 +1,7 @@
+import styles from './inputText.module.css';
+
+const InputText = ({ ...props }: React.InputHTMLAttributes<HTMLInputElement>) => {
+  return <input className={styles.input} {...props} />;
+};
+
+export default InputText;
